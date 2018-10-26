ff375dc190ac583382dfbcfba51d34e88048b3b8
yuriy.evseenko@gmail.com <yuriy.evseenko@gmail.com>
2018-10-26 16:17:00 +0300

    adding db

---
+++ config/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+  url : mongo eu-cdbr-west-02.cleardb.net -p heroku_831869270943f5e -u bf334996da4052 --password f955ea08
+};
--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
+const mysql = require('mysql');
 const bodyParser = require('body-parser');
 const app = express();
 
 
 const port = 8000;
-
 require('./app/routes')(app, {});
+app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(port, () => {
   console.log('We are live on ' + port);
 });
