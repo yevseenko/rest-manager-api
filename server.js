bb7d625163a8a9bd33804f90b97289c824379ac3
yuriy.evseenko@gmail.com <yuriy.evseenko@gmail.com>
2018-10-29 20:20:01 +0200

    server

--- server.js
+++ server.js
@@ -2,12 +2,33 @@ import express from 'express';
 import mysql from 'mysql';
 import bodyParser from 'body-parser';
 import routes from './app/routes';
+import config from 'config';
 
 const port = 8000;
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.listen(port, () => {
   console.log('We are live on ' + port);
 });
 
 routes(app, {});
+
+const dbConfig = config.get('user.dbConfig');
+
+const connection = mysql.createConnection(dbConfig);
+
+connection.connect(function (err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+
+  console.log('connected as id ' + connection.threadId);
+});
+
+connection.query('SELECT * from *', function (_error, results, fields) {
+  console.log(results);
+  console.log(fields);
+})
