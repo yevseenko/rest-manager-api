c5b3e34660afd31169249fafc2f600adc1ef0340
yevseenko <yuriy.evseenko@gmail.com>
2019-03-26 23:05:41 +0200

    ...

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import routes from './app/routes';
 import router from './router';
 import config from 'config';
 
@@ -10,10 +9,9 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-routes(app);
-
 app.use(router);
 
 app.listen(config.server.port, config.server.host, function () {
   console.log(`We are live on ${config.server.host}:${config.server.port}`);
 });
+
