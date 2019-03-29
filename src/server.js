805b24c400bbbd777aaf2dcaf082ef826db4d0b6
yuriy.evseenko@gmail.com <yuriy.evseenko@gmail.com>
2019-03-29 17:48:11 +0200

    allow cors

--- src/server.js
+++ src/server.js
@@ -11,6 +11,12 @@ app.use(bodyParser.urlencoded({
 
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(router);
 
 app.listen(config.server.port, config.server.host, function () {
